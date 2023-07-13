1a4beec980e045447ea7bb232d8f49bff04775b5
sontn <nts6920@gmail.com>
2023-07-13 14:35:48 +0700

    feat: update gradle

--- app/src/main/java/com/nts/tichhopvoipsdk/MainActivity.java
+++ app/src/main/java/com/nts/tichhopvoipsdk/MainActivity.java
@@ -2,40 +2,45 @@ package com.nts.tichhopvoipsdk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.g99.linphone.activities.voip.CallActivity;
 import com.g99.voip.CallbackValue;
 import com.g99.voip.SDKManager;
 import com.g99.voip.UserInfo;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
     Button button;
-
+    String mobile = "0973385525";
+    int merchantId = 1;
+    String customer = "Nguyen Duc Vinh";
+    String identification = "033080007111";
+    String uID = "999999999";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.btn);
 
+        Intent intent = new Intent(this, CallActivity.class);
 
         button.setOnClickListener(view -> {
 
             UserInfo userInfo = new UserInfo();
-            userInfo.setMobile("0973385525");
-            userInfo.setMerchant_id(1);
-            userInfo.setCustomer("Nguyen Duc Vinh");
-            userInfo.setIdentification_no("033080007111");
-            userInfo.setUid("999999999");
+            userInfo.setMobile(mobile);
+            userInfo.setMerchant_id(merchantId);
+            userInfo.setCustomer(customer);
+            userInfo.setIdentification_no(identification);
+            userInfo.setUid(uID);
 
-            SDKManager.Companion.init(getApplication())
-                    .setUserInfo(userInfo)
-                    .setCallbackListener((callbackValue) -> {
+            SDKManager.Companion.init(getApplication()).setUserInfo(userInfo).setCallbackListener((callbackValue) -> {
                         if (callbackValue == CallbackValue.REQUIRED_LOGIN) {
-//                            replaceLoginActivity();
+                            // replaceLoginActivity
+                            Toast.makeText(this, "replaceLoginActivity", Toast.LENGTH_SHORT).show();
                         } else {
                             Snackbar.make(view, "No support", Snackbar.LENGTH_LONG).show();
                         }
